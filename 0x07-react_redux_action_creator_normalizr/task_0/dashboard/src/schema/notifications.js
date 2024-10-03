253ecc94af9b336d8cc1fdad378b23cfce565254
willys12 <ben.willys92@gmail.com>
2024-10-03 11:44:54 +0300

    Read data from JSON

---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,8 @@
+import notificationData from "../../../../notifications.json";
+
+// Create a function named getAllNotificationsByUser that 
+//accepts userId as an argument
+//The function should return a list containing all the context objects from the notifications.json data when the author id is the same as the userId
+export const getAllNotificationsByUser = (userID) => {
+    return notificationData.filter(notification => notification.authorId === userID).map(notification => notification.context);
+};
